4e4bc48b05358145aae8b4ac9a6d2cf52c7a4881
tzongw <tzongw@gmail.com>
2014-01-06 00:08:56 +0800

    Revert "delete data"
    This reverts commit a329e5c83f28e8d63d91054d37d76999e35ceec1.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	int arr[] = { 2, -5, 9, 7, -2, 5, 3, 1, 0, -3, 7 };
+	int arr[] = { 1, 2, -5, 9, 7, -2, 5, 3, 1, 0, -3, 7 };
 	vector<int> num(arr, arr + sizeof(arr) / sizeof(arr[0]));
 	vector<int> p_num;
 	copy_if(num.begin(), num.end(), back_inserter(p_num), bind2nd(greater<int>(), 0));
